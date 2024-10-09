40ffdc931b5c7df1770081c78db9d7903f727291
Liyo555 <gamingliyo82@gmail.com>
2024-10-09 19:46:43 +0530

    Update config.js

--- config.js
+++ config.js
@@ -5,7 +5,7 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-SESSION_ID: process.env.SESSION_ID || "ibIWEQJI#eXejwWyh96nrkanLLxTiWgbFno14GChmhBbqV6I4JvE",
+SESSION_ID: process.env.SESSION_ID || "FyN2BBqC#_omP8ISQ4BIeTgtZPW2haOhvinj_lzmvpnu9r-nAtuU",
 AUTO_READ_STATUS: process.env.AUTO_READ_STATUS || "true",
 MODE: process.env.MODE || "public",
 AUTO_VOICE: process.env.AUTO_VOICE || "true",
